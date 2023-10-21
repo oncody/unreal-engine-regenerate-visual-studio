74fae1684131b5352cbb03c1ddce60a1283ea4d5
Cody Smith <37386486+oncody@users.noreply.github.com>
2023-10-21 12:43:35 -0600

    changes

--- regenerate-visual-studio-project.bat
+++ regenerate-visual-studio-project.bat
@@ -1,32 +1,31 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 set "engineVersion=UE_5.3"
 set "enginePath=D:\Programs\Unreal Engine\%engineVersion%\Engine"
 
-set "projectPath=D:\Code\unreal-engine-multiplayer-plugin-menu-system"
-set "projectName=MultiplayerMenu.uproject"
-
-
+set "projectPath=D:\Code\unreal-engine-multiplayer-shooter"
+set "projectName=Blaster.uproject"
 
 REM Define the directories to remove
-set "dirsToRemove=Plugins\MultiplayerSessions\Binaries 
-Plugins\MultiplayerSessions\Intermediate 
-Binaries 
-Intermediate 
-Saved"
+set "dirsToRemove=Plugins\MultiplayerSessions\Binaries Plugins\MultiplayerSessions\Intermediate Binaries Intermediate Saved"
 
-REM Loop through and remove each directory
+REM Loop through and print the directory to be checked or removed
 for %%d in (%dirsToRemove%) do (
-    set "dirPath=%projectPath%\%%d"
+    set "dirPath=!projectPath!\%%d"
+    echo Checking directory "!dirPath!"
     if exist "!dirPath!" (
         echo Removing directory "!dirPath!"
         rmdir /s /q "!dirPath!"
+    ) else (
+        echo Directory "!dirPath!" does not exist.
     )
 )
 
+pause
+
 REM Run the UnrealBuildTool command
-"%enginePath%\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe" -projectfiles -project=%projectPath%\%projectName%
+"%enginePath%\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe" -projectfiles -project="%projectPath%\%projectName%"
 
 echo Script completed.
 pause
